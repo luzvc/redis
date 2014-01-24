59ccc59627d5ca7ff5a7c093716113b53651b152
Bruno Azisaka Maciel <bruno@azisaka.com.br>
2014-01-24 07:12:00 -0200

    Redis with a conf template

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,4 +6,15 @@
 #
 include_recipe "apt"
 
+service "redis" do
+  supports restart: true
+end
+
 package "redis-server"
+
+template "/etc/redis/redis.conf" do
+  source "redis.conf.erb"
+  user "root"
+  group "root"
+  notifies :restart, "service[redis]"
+end
